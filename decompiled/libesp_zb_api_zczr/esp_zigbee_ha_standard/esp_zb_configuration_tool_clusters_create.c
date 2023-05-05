320931d9c3dbbdb49b99784900556461051584c3
blobmaster <blobmaster.crux120@passmail.net>
2023-05-05 20:30:36 +0800

    decompile: upstream fbb183ba2e7004edff49ad5740e127aacad3a0f5 (2023-05-05) -- 1 files changed
    Upstream commit: https://github.com/espressif/esp-zigbee-sdk/commit/fbb183ba2e7004edff49ad5740e127aacad3a0f5
    
    Upstream subject: esp-zigbee-lib: update lib for support ieee802154 open source(a8dc9c7)
    
    Archives decompiled: 2

--- decompiled/libesp_zb_api_zczr/esp_zigbee_ha_standard/esp_zb_configuration_tool_clusters_create.c
+++ decompiled/libesp_zb_api_zczr/esp_zigbee_ha_standard/esp_zb_configuration_tool_clusters_create.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit baa2c0427a754013932b7b4a3bc21cee014ddd24
- * https://github.com/espressif/esp-zigbee-sdk/commit/baa2c0427a754013932b7b4a3bc21cee014ddd24
- * Upstream date: 2023-02-10 16:01:53 +0800
- * Upstream subject: examples: provide new API for channel set(5818e5d)
+ * Last changed at upstream commit fbb183ba2e7004edff49ad5740e127aacad3a0f5
+ * https://github.com/espressif/esp-zigbee-sdk/commit/fbb183ba2e7004edff49ad5740e127aacad3a0f5
+ * Upstream date: 2023-05-05 20:30:36 +0800
+ * Upstream subject: esp-zigbee-lib: update lib for support ieee802154 open source(a8dc9c7)
  * Source: libesp_zb_api_zczr -> esp_zigbee_ha_standard.o -> esp_zb_configuration_tool_clusters_create
  *
  * (C) Espressif, Apache License 2.0.
@@ -23,16 +23,8 @@ undefined4 esp_zb_configuration_tool_clusters_create(int param_1)
   esp_zb_cluster_list_add_identify_cluster(uVar1,uVar2,1);
   uVar2 = esp_zb_zcl_attr_list_create(3);
   esp_zb_cluster_list_add_identify_cluster(uVar1,uVar2,2);
-  uVar2 = esp_zb_groups_cluster_create(param_1 + 4);
-  esp_zb_cluster_list_add_groups_cluster(uVar1,uVar2,1);
-  uVar2 = esp_zb_scenes_cluster_create(param_1 + 6);
-  esp_zb_cluster_list_add_scenes_cluster(uVar1,uVar2,1);
-  uVar2 = esp_zb_on_off_cluster_create(param_1 + 0xc);
-  esp_zb_cluster_list_add_on_off_cluster(uVar1,uVar2,1);
-  uVar2 = esp_zb_level_cluster_create(param_1 + 0xd);
-  esp_zb_cluster_list_add_level_cluster(uVar1,uVar2,1);
-  uVar2 = esp_zb_color_control_cluster_create(param_1 + 0xe);
-  esp_zb_cluster_list_add_color_control_cluster(uVar1,uVar2,1);
+  uVar2 = esp_zb_zcl_attr_list_create(4);
+  esp_zb_cluster_list_add_groups_cluster(uVar1,uVar2,2);
   return uVar1;
 }
 
